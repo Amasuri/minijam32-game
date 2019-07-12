db12de8022f026820be363b220ec950d94950e5a
keiondesu96 <keiondesu96@gmail.com>
2019-07-12 08:31:03 +0300

    basic input

---
+++ MiniJam32Game/Code/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace BPO.Minijam32.Player
+{
+    /// <summary>
+    /// A class which takes input and produces action based on current screen and state. Must know what's going on in the game.
+    ///
+    /// No player information is being hold there - go to PlayerData for that.
+    /// </summary>
+    static public class PlayerController
+    {
+        private const Keys keyUp = Keys.W;
+        private const Keys keyDown = Keys.S;
+        private const Keys keyLeft = Keys.A;
+        private const Keys keyRight = Keys.D;
+
+        private static KeyboardState keyState;
+        private static KeyboardState oldKeyState;
+
+        static public void Update(Minijam32 game)
+        {
+            keyState = Keyboard.GetState();
+
+            if (oneKeyPress(keyUp))
+                PlayerDataManager.Move(new Point(0, -1));
+            else if (oneKeyPress(keyDown))
+                PlayerDataManager.Move(new Point(0, +1));
+            else if (oneKeyPress(keyLeft))
+                PlayerDataManager.Move(new Point(-1, 0));
+            else if (oneKeyPress(keyRight))
+                PlayerDataManager.Move(new Point(1, 0));
+
+            oldKeyState = keyState;
+        }
+
+        static private bool oneKeyPress(Keys key)
+        {
+            return keyState.IsKeyDown(key) && oldKeyState.IsKeyUp(key);
+        }
+    }
+}
---
+++ MiniJam32Game/Code/Player/PlayerDataManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPO.Minijam32.Player
+{
+    /// <summary>
+    /// Your standard data manager: saving, remembering what have been done, etc.
+    ///
+    /// Is __internal__ because data managment belongs to Player assembly in general, not to other entities.
+    /// PlayerController invoked many of it's calls and manages interfacing via other stuff.
+    /// This all is to simplify debugging.
+    /// </summary>
+    static internal class PlayerDataManager
+    {
+        static private Point tilePosition;
+
+        static internal void Move(Point move)
+        {
+            tilePosition += move;
+        }
+    }
+}
