594e8be533795d081a37bc38cb8fe23608291ce4
keiondesu96 <keiondesu96@gmail.com>
2019-07-14 06:36:55 +0300

    soundplayer tweaks & new sound

--- MiniJam32Game/Code/Music/SoundPlayer.cs
+++ MiniJam32Game/Code/Music/SoundPlayer.cs
@@ -15,6 +15,7 @@ namespace BPO.Minijam32.Music
     {
         public enum Type
         {
+            BombExplosion,
         }
 
         static private Dictionary<Type, SoundEffect> sounds;
@@ -23,13 +24,18 @@ namespace BPO.Minijam32.Music
         {
             sounds = new Dictionary<Type, SoundEffect>
             {
+                { Type.BombExplosion, game.Content.Load<SoundEffect>("res/sound/explosion0") },
             };
         }
 
         static public void PlaySound(Type type)
         {
+            float volume = 1.0f; //0.0f is silence, 1.0f is full volume
+            float pitch = 0.0f; //-1.0f (down one octave), 1.0f (up one octave), 0.0f is normal pitch.
+            float pan = 0.0f; //-1.0f (full left) to 1.0f (full right). 0.0f is centered.
+
             if (sounds.ContainsKey(type))
-                sounds[type].Play();
+                sounds[type].Play(volume, pitch, pan);
         }
     }
 }
